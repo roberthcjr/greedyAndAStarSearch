9b4b873d2642945f20c746f8ba1afad0c996313b
Robert <roberthcjr1999@gmail.com>
2024-04-14 21:43:13 -0300

    refactor(main): corrects method name

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-from searchMethods.gredyFirst import gredyFirstSearch
+from searchMethods.greedyFirst import greedyFirstSearch
 from shared.distancias import distanciasRodoviarias, distanciasBucharest
 
 # Define the start and goal nodes
 start = "Arad"
 goal = "Bucharest"
 
-# Call the Gredy Best First Search function
-path, cost = gredyFirstSearch(start, goal, distanciasRodoviarias, distanciasBucharest)
+# Call the Greedy Best First Search function
+path, cost = greedyFirstSearch(start, goal, distanciasRodoviarias, distanciasBucharest)
 
 # Print the path and the cost
 print("Path:", path)
