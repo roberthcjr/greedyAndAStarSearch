dad644ac4c84ded459d9397f50efb02cc56033af
Robert <roberthcjr1999@gmail.com>
2024-04-22 08:34:06 -0300

    fix(getters): removes useless parameters

--- src/utils/getters.py
+++ src/utils/getters.py
@@ -1,4 +1,4 @@
-def getPathAndCost(searchType, start, city, minor, neighbours, pathAux, costAux):
+def getPathAndCost(start, city, neighbours, pathAux, costAux):
     path = [start] + pathAux
     cost = neighbours[city] + costAux
     return path, cost
