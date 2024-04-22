886503bdd9fdfa93418bd4011901ae0cfafd3036
Robert <roberthcjr1999@gmail.com>
2024-04-22 08:31:52 -0300

    feat(main): implements time processing calculation

--- src/main.py
+++ src/main.py
@@ -1,19 +1,28 @@
 from helpers.search import search
 from shared.distancias import distanciasRodoviarias, distanciasBucharest
 from helpers.map import drawMap, openMap
+import time
 
 start = input("Digite a cidade de origem: ")
 while start not in distanciasBucharest:
     start = input("Cidade não encontrada, digite outra: ")
 goal = "Bucharest"
 
-greedy = search(0, start, goal, distanciasRodoviarias, distanciasBucharest)
-greedy["path"] += [goal]  
 
+inicioGreedy = time.time()
+greedy = search(0, start, goal, distanciasRodoviarias, distanciasBucharest)
+greedy["path"] += [goal]
+fimGreedy = time.time()
+
+inicioAStar = time.time()
 aStar = search(1, start, goal, distanciasRodoviarias, distanciasBucharest)
 aStar["path"] += [goal]
+fimAStar = time.time()
 
-drawMap(greedy, aStar, start, goal)
+print("Tempo de execução do Guloso:", fimGreedy - inicioGreedy)
+print("Tempo de execução do AStar:", fimAStar - inicioAStar)
+
+drawMap(greedy, aStar)
 
 # Print the path and the cost
 print("O caminho feito pelo método guloso:", greedy["path"])
