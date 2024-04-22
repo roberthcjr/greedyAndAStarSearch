ac8ba8111f22d1c2261d008d82fa5d986e4dfc1d
Robert <roberthcjr1999@gmail.com>
2024-04-22 08:32:46 -0300

    refactor(search): changes code order

--- src/helpers/search.py
+++ src/helpers/search.py
@@ -1,21 +1,21 @@
-from utils.comparations import findMinorDistanceAndCity, isAStar, SEARCH_TYPES
+from utils.comparations import findCityWithMinDistance, isAStar, SEARCH_TYPES
 from utils.getters import getPathAndCost
 
 
 # This file contains the search function that is used to find the path between two cities
 def search(searchType, start, goal, frontiers, distancies, visited = None):
-    if visited is None:
-        visited = []
     path = []
     cost = 0
-    distanciesAux = distancies.copy()
-
     if start == goal:
         return {
             "path": path,
             "cost": cost,
             "searchType": SEARCH_TYPES[searchType]
         }
+    
+    if visited is None:
+        visited = []
+    distanciesAux = distancies.copy()
 
     visited.append(start)
     neighbours = frontiers[start]
@@ -25,11 +25,11 @@ def search(searchType, start, goal, frontiers, distancies, visited = None):
             neighbour: distancies[neighbour] + neighbours[neighbour] for neighbour in neighbours
         })
 
-    city, minor = findMinorDistanceAndCity(neighbours, distanciesAux, visited)
+    city = findCityWithMinDistance(neighbours, distanciesAux, visited)
     
     aux = search(searchType, city, goal, frontiers, distancies, visited)
 
-    path, cost = getPathAndCost(searchType, start, city, minor, neighbours, aux["path"], aux["cost"])
+    path, cost = getPathAndCost(start, city, neighbours, aux["path"], aux["cost"])
 
     return {
             "path": path,
